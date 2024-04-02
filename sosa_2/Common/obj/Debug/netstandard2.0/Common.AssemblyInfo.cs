c25f80bc2119e32b219de9d322706c64882aabd6
Diman-byte <75217873+Diman-byte@users.noreply.github.com>
2024-04-02 10:51:30 +0300

    проба

--- sosa_2/Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
+++ sosa_2/Common/obj/Debug/netstandard2.0/Common.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Common")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce9f4dfd550bd08c4a1c6ee522571c9602f7ded")]
 [assembly: System.Reflection.AssemblyProductAttribute("Common")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Common")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
